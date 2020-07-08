f4d239cb0f545ea5cd8d591e233ff3a80bc7d05b
47oo <136698493@qq.com>
2020-07-08 13:02:36 +0800

    删除zoo.conf中多余的ip地址

--- mesos/labs/init/hosts_init.sh
+++ mesos/labs/init/hosts_init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # 为/etc/hosts追加主机
 cat >> /etc/hosts<<EOF
-192.168.205.11 master
-192.168.205.12 agent1
-192.168.205.13 agent2
+192.168.205.11 mesos-master
+192.168.205.12 mesos-agent1
+192.168.205.13 mesos-agent2
+192.168.205.101 zk1
+192.168.205.102 zk2
+192.168.205.103 zk3
 EOF
